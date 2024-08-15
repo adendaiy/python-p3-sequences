cfaaec6575b7ae67bd2cf1aabe636c0284c1078b
adendaiy <adenabdib@gmail.com>
2024-08-15 22:03:33 +0100

    this is my 3rd python lab sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
+# def print_fibonacci(length):
+#     pass
+
+# def print_fibonacci():
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    
+    for i in range(length):
+        if i == 0:
+            fibonacci_sequence.append(0)
+        elif i == 1:
+            fibonacci_sequence.append(1)
+        else:
+            next_value = fibonacci_sequence[i-1] + fibonacci_sequence[i-2]
+            fibonacci_sequence.append(next_value)
+    
+    print(fibonacci_sequence)
+
+print_fibonacci(9)
